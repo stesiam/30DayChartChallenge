4513a64e0064c1a01dfb2fa3c78e0e4c03f86b01
stesiam <52320488+stesiam@users.noreply.github.com>
2024-03-23 19:12:41 +0200

    Add day 1 and 4 chart challenge

---
+++ 2024/day1/day1.R
@@ -0,0 +1,130 @@
+library(dplyr)
+library(ggplot2)
+library(ggtext)
+library(glue)
+library(ggforce)
+
+library(showtext)
+library(sysfonts)
+
+## Load fonts
+
+sysfonts::font_add_google("Oswald", "title")
+sysfonts::font_add_google("Josefin Slab","js")
+sysfonts::font_add_google("Cabin Condensed","cc")
+sysfonts::font_add_google("Ubuntu Condensed", "uc")
+
+#sysfonts::font_add_google("Gentium Plus", "gp")
+sysfonts::font_add('gp',"/home/stelios/Downloads/Gentium_Plus/GentiumPlus-Regular.ttf")
+
+sysfonts::font_add('fb', '/home/stelios/Downloads/fontawesome-free-6.4.0-desktop/otfs/Font Awesome 6 Brands-Regular-400.otf')
+sysfonts::font_add('fs', '/home/stelios/Downloads/fontawesome-free-6.4.0-desktop/otfs/Font Awesome 6 Free-Solid-900.otf')
+
+
+showtext_auto()
+showtext::showtext_opts(dpi = 300)
+
+## Data
+
+# Source: https://selfservice.gr/panorama-ton-ellinikon-souper-market-2023-i-chrimatooikonomiki-eikona-42-alysidon-souper-market-to-2022/
+
+sklavenitis_sales_2022 = 3714124000
+sm_sales = 11417257000
+
+col  = c("grey90", "#c6a464")
+
+ratio = sklavenitis_sales_2022/sm_sales
+
+data = data.frame(
+  "Brand" = c("Sklavenitis", "Other"),
+  "Value" = c(ratio, 1-ratio),
+  "Sales" = c(sklavenitis_sales_2022, sm_sales - sklavenitis_sales_2022),
+  "col"  = c("#c6a464", "grey95")
+)
+
+title_text = glue("<b>Supermarkets' Sales in Greece (2022)</b>")
+subtitle_text = glue( "According to the latest survey of IELKA, the total sales of Greek 
+    supermarkets are <br> amounted to € <b>11.2</b> bil. <b><span style = 'color: #c6a464;'>Sklavenitis</span></b> is by far the most
+    prevalent of all its competitors <br> with € <b>3.7</b> bil. in sales (32% of total market)." )
+caption_text = "30 Day Chart Challenge, Day 1 (2024) | <b> Data:</b> Panorama of The Greek Supermarkets, selfservice.gr<br><span style='font-family:fb;'  >&#xf09b;</span> <b>stesiam</b>, 2024"
+
+
+p = ggplot(data) +
+  geom_arc_bar(aes(
+    x0 = 0,
+    y0 = 0,
+    r0 = 0.6,
+    r = 1,
+    amount = Value,
+    fill = Brand
+  ),
+  stat = "pie",
+  size = 1,
+  color = "#FFFFFF"
+  ) +
+  geom_richtext(
+    x = c(1.25, -1.25),
+    y = c(-0.3, 0.3),
+    aes(label = paste0(
+      glue("<b>{Brand}</b><br>"),
+      "<strong>EUR€ ",
+      round(Sales/1e9, 2),
+      "B</strong>", "<br>",
+      round(100 * Value / sum(Value), 2),
+      "%"
+    )),
+    size = 10 / .pt,
+    fill = NA,
+    label.color = NA
+  ) +
+  geom_richtext(
+    x = 0,
+    y = 0,
+    label = paste0(
+      "<span style='font-family:fs; color:#222021; font-size:30pt;'  >&#xf07a;</span><br>",
+      "Total Sales",
+      "<br><strong>EUR€ ",
+      round(sum(data$Sales) / 1e9, 2),
+      "B</strong>"
+    ),
+    size = 14 / .pt,
+    fill = NA,
+    label.color = NA
+  ) +
+  labs(
+    title = title_text,
+    subtitle = subtitle_text,
+    caption = caption_text
+  ) +
+  scale_x_continuous(expand = expansion(c(0.3, 0.5))) +
+  scale_fill_manual(values = c("Sklavenitis" = "#c6a464",
+                      "Other" = "grey80")) +
+  theme_void(base_size = 11.5)  +
+  theme(
+    plot.title = element_markdown(family = "js",
+                                  margin = margin(t = 10, b = 5), 
+                                  hjust = 0.5, 
+                                  color = "black",face = "bold"),
+    plot.title.position = "plot",
+    plot.subtitle = element_markdown(family = "serif",
+                                     margin = margin(t = 5, l = 10, r = 10, b = 5),
+                                     lineheight = 1.1,
+                                     color = "black"),
+    plot.background = element_rect(fill = "white", color = "white"),
+    panel.background = element_rect(fill = "white", color = "white"),
+    plot.caption = element_markdown(family = "title", margin = margin(t = 5, r = 5, b = 4), 
+                                    lineheight = 1.4,
+                                    color = "black", size = 8,
+                                    hjust = 0.5),
+    plot.margin = margin(l=8, r=8),
+    legend.position = "none"
+  )
+
+ggsave(
+  filename = "2024/day1/day1-2024-cc.png",
+  plot = p,
+  device = "png",
+  height = 4,
+  width = 6)
+
+
---
+++ 2024/day4/day4.R
@@ -0,0 +1,165 @@
+library(ggplot2)
+library(rvest)
+library(dplyr)
+library(tidyr)
+library(ggimage)
+library(ggtext)
+
+library(showtext)
+library(sysfonts)
+
+## Load fonts
+
+sysfonts::font_add_google("Oswald", "title")
+sysfonts::font_add_google("Josefin Slab","js")
+sysfonts::font_add_google("Cabin Condensed","cc")
+sysfonts::font_add_google("Ubuntu Condensed", "uc")
+
+#sysfonts::font_add_google("Gentium Plus", "gp")
+sysfonts::font_add('gp',"/home/stelios/Downloads/Gentium_Plus/GentiumPlus-Regular.ttf")
+
+sysfonts::font_add('fb', '/home/stelios/Downloads/fontawesome-free-6.4.0-desktop/otfs/Font Awesome 6 Brands-Regular-400.otf')
+sysfonts::font_add('fs', '/home/stelios/Downloads/fontawesome-free-6.4.0-desktop/otfs/Font Awesome 6 Free-Solid-900.otf')
+
+
+showtext_auto()
+showtext::showtext_opts(dpi = 300)
+
+
+# Custom function to get club emblems
+get_teams_emblem = function(team_name){
+  if (team_name == "AEL"){
+    team_name = "Athlitiki_Enosi_Larissa"
+  } else if (team_name == "OFI"){
+    team_name = "OFI_Crete" 
+  }
+  url = paste0("https://en.wikipedia.org/wiki/", team_name, "_F.C.")
+  
+  image_url = read_html(url) |>
+    html_element("body") |>
+    html_element(".mw-content-container") |>
+    html_element(".infobox") |>
+    html_element("img") |>
+    html_attr("src")
+  
+  x = paste0("https:", image_url)
+  download.file(x, destfile = paste0("2024/day2/team_logos/", team_name, ".png"))
+}
+
+
+url = "https://en.wikipedia.org/wiki/Greek_Football_Cup"
+
+
+read_html(url)
+
+
+greek_cup_data = url |>
+  read_html() %>%
+  html_element("body") %>%
+  html_elements(".div-col") %>%
+  html_elements("li") %>%
+  html_text2() %>%
+  as.data.frame() %>%
+  setNames(c("Var1")) %>%
+  separate(Var1, into = c("Season", "Teams"), sep = ": ") %>%
+  separate(Teams, into = c("Teams", "TimesWon"), sep = "\\(") %>%
+  mutate(
+    TimesWon = stringr::str_remove(TimesWon, "\\)"),
+    Teams = stringr::str_trim(Teams)
+  )
+greek_cup_data$Teams[greek_cup_data$Teams == "–"] <- "None"
+greek_cup_data$Teams[greek_cup_data$Teams == "Ethnikos"] <- "Ethnikos_Piraeus"
+  
+
+teams = greek_cup_data$Teams %>% unique()
+for (team in teams){
+  if(team == "None"){
+    next
+  }
+  get_teams_emblem(team)
+}
+
+# Make data frame with emblems
+
+logos = data.frame(
+  "Teams" = c("AEK", "Ethnikos_Piraeus", "Panathinaikos", "Olympiacos", "None",
+              "Aris", "PAOK", "Iraklis", "Panionios", "Kastoria","AEL","OFI"),
+  "Logo" = c("AEK.png", "Ethnikos_Piraeus.png", "Panathinaikos.png", "Olympiacos.png",
+             "", "Aris.png", "PAOK.png", "Iraklis.png", "Panionios.png", "Kastoria.png",
+             "Athlitiki_Enosi_Larissa.png", "ofi.png")
+)
+dest = "2024/day2/team_logos/"
+logos$Logo = paste0(dest, logos$Logo)
+
+
+## Join Logo data frame with data 
+
+clean_data = left_join(x = greek_cup_data, y = logos, by = "Teams") %>%
+ dplyr::filter(Teams != "None")
+clean_data$id = rep(20:1, 4)
+## Viz
+
+freq_cups_won_by_team = clean_data %>%
+  group_by(Teams) %>%
+  summarise(n = n()) %>%
+  arrange(-n) 
+
+freq_cups_won_by_team_big5 = freq_cups_won_by_team %>%
+  mutate(
+    Big5 = ifelse(Teams %in% c("Panathinaikos", "Olympiacos", "AEK",
+                               "PAOK", "Aris"), TRUE, FALSE),
+    total_cups = sum(n)
+  ) %>%
+  group_by(Big5) %>%
+  summarise(pct = sum(n)/total_cups)
+
+freq_cups_won_by_team$n[1]
+
+freq_cups_won_by_team %>% nrow()
+str_remove(clean_data$Season, "-.*")
+
+# Problem: I had to set x and y values. I set them as constants (0 and 1)
+clean_data
+
+title_text = "<b> <span style='font-family:fs;'  >&#xf091;</span> Greek Football Cup</b> (1931 - 2022) <span style='font-family:fs;'  >&#xf091;</span>"
+subtitle_text =  glue("<b><span style = 'color:#D0061F;'>Olympiacos</span></b> is the team that has won <b>GFC</b> most times ({freq_cups_won_by_team$n[1]}) followed by <b><span style = 'color:#007841;'>Panathinaikos</span><br></b>
+    and <b><span style = 'color:#c8a951;'>AEK Athens</span></b> with {freq_cups_won_by_team$n[2]} and {freq_cups_won_by_team$n[3]} cups, respectively. In total, {freq_cups_won_by_team %>% nrow()} teams have succeeded to <br>
+    win the cup the last 80 cup seasons of which only <b>10% (8)</b> are from teams outside the<br> so-called Big5 (Olympiacos, Panathinaikos, AEK Athens, PAOK, Aris)")
+caption_text = "30 Day Chart Challenge, Day 2 (2024) | <b> Data:</b> Wikipedia<br><span style='font-family:fb;'  >&#xf09b;</span> <b>stesiam</b>, 2024"
+
+p = ggplot(data = clean_data, aes(0,1))+
+  geom_image(aes(image=Logo),size=.6) +
+  facet_wrap(~Season, nrow = 5) +
+  labs(
+    title = title_text,
+    subtitle = subtitle_text,
+    caption = caption_text
+  ) +
+  theme_void() +
+  theme(
+    plot.margin = margin(l = 10, r = 10),
+    panel.background = element_rect(fill = "black", color = "black"),
+    plot.background = element_rect(fill = "black"),
+    text = element_text(color = "white"),
+    strip.text = element_markdown(size = 4.5,face = "bold", family = "serif"),
+    plot.caption = element_markdown(family = "title", margin = margin(t = 5, r = 5, b = 4), 
+                                    lineheight = 1.4,
+                                    color = "white", size = 8,
+                                    hjust = 0.5),
+    plot.title = element_markdown(family = "js",
+                                  margin = margin(t = 10, b = 5), 
+                                  hjust = 0.5, 
+                                  color = "white",face = "bold"),
+    plot.title.position = "plot",
+    plot.subtitle = element_markdown(family = "js",
+                                     margin = margin(t = 5, l = 10, r = 10, b = 5),
+                                     lineheight = 1.1,
+                                     color = "white")
+)
+
+ggsave(
+  filename = "2024/day4/day4-2024-cc.png",
+  plot = p,
+  device = "png",
+  height = 4,
+  width = 6)
