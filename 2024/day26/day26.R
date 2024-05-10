9a47a4d920c39961d1ccd9d2442159534246e05d
stesiam <52320488+stesiam@users.noreply.github.com>
2024-05-11 00:30:46 +0300

    Add day26 and day28 in 30DCC (2024)

---
+++ 2024/day26/day26.R
@@ -0,0 +1,151 @@
+# Data Source: Kaggle Datasets
+# Data URL: https://www.kaggle.com/datasets/muhammadtalhaawan/ai-5000-tools-2023/data
+
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(glue)
+library(ggtext)
+library(showtext)
+library(sysfonts)
+library(forcats)
+
+library(ggh4x)
+## Load fonts
+
+sysfonts::font_add_google("Outfit", "title")
+sysfonts::font_add_google("Ubuntu Condensed", "uc")
+sysfonts::font_add_google("Jost", "jost")
+
+sysfonts::font_add('fb', '/home/stelios/Downloads/fontawesome-free-6.4.0-desktop/otfs/Font Awesome 6 Brands-Regular-400.otf')
+sysfonts::font_add('fs', '/home/stelios/Downloads/fontawesome-free-6.4.0-desktop/otfs/Font Awesome 6 Free-Solid-900.otf')
+
+
+showtext_auto()
+showtext::showtext_opts(dpi = 300)
+
+# Import dataset
+
+all_ai_tool <- read_csv("2024/day26/all_ai_tool.csv")
+
+
+e=all_ai_tool |>
+  setNames(c("Name", "Description", "ChargingType", "usedFor", "Charges", 
+             "Review", "URL", "Category")) |>
+  tidyr::drop_na(Charges) |>
+  mutate(
+    ChargesPER = case_when(
+      grepl("mo", Charges) ~ "Monthly",
+      grepl("wk", Charges) ~ "Weekly",
+      grepl("word|request|credit)", Charges) ~ "Other",
+      grepl("hour", Charges) ~ "Hourly",
+      grepl("year", Charges) & grepl("yr", Charges) ~ "yearly",
+      grepl("second", Charges) ~ "second",
+      grepl("free", tolower(Charges)) ~ "free",
+      TRUE ~ "Other"
+  )) |>
+  mutate(
+    Charges = stringr::str_remove_all(Charges, "\\$"),
+    Charges = stringr::str_remove_all(Charges, "\\/.*"),
+    Charges = stringr::str_remove_all(Charges, "[a-z, A-Z]"),
+    Charges = stringr::str_remove_all(Charges, "-"),
+    Charges = stringr::str_trim(Charges),
+    Charges = as.numeric(Charges)
+) |>
+  tidyr::drop_na(Charges) |>
+  dplyr::filter(ChargesPER == "Monthly") |>
+  dplyr::filter(
+    Charges >0
+  ) |>
+  mutate(
+    Charges = case_when(
+      ChargesPER == "Yearly" ~ Charges/12,
+      ChargesPER == "Weekly" ~ Charges*4,
+      ChargesPER == "Hourly" ~ Charges*24*30,
+      ChargesPER == "free" ~ Charges*0,
+      ChargesPER == "second" ~ Charges*60*60*24*30,
+      TRUE ~ Charges
+    )
+  )|>
+  dplyr::filter(ChargesPER != "other") |>
+  group_by(Category) |>
+  summarise(median = median(Charges),
+            q1 = quantile(Charges, probs = 0.25),
+            q3 = quantile(Charges, probs = 0.75),
+            n = n(),
+            max = max(Charges),
+            min = min(Charges)) |>
+  mutate(CatCharges = case_when(
+    median < 11.6 ~ "$",
+    median < 15.5 ~ "$$",
+    TRUE ~ "$$$"
+  )) |>
+  mutate(
+    Category = stringr::str_to_title(Category)
+  ) |>
+  dplyr::filter(Category != "Other")
+
+
+
+# Plot texts
+
+title_text = glue("<b>Pricing of <span style='color:black;'>AI</span> Tools by Purpose</b>")
+subtitle_text = glue("<b>Artificial Intelligence</b> (AI) has already intervened in our lifes and it has helped us make our lifes <br>easier.
+                     Till today over 5000 AI tools have been recorded for various uses such as Email Preparation, <br>Grammar
+                     checking, Coding assistance, etc. Almost half of them offer paid plans. Focusing on those <br>services, I conclude that
+                     AI for <b>coding</b>, <b>video</b> and <b> business</b> purposes <b><span style='font-family:title; color:#ff7f50;' >are the most expensive </span></b>.")
+caption_text = "*NOTE:* The pricing is on a monthly basis. Also the categorization of pricing is based on median charges.<br>30 Day Chart Challenge, Day 26 (2024) | <b> Data:</b> Kaggle Datasets <br><span style='font-family:fb;'  >&#xf09b;</span> <b>stesiam</b>, 2024"
+
+
+
+bg_gradient <- grid::linearGradient(colours = rev(MetBrewer::met.brewer("Kandinsky")[3:4]))
+
+
+design <- c(
+"AABBCCDD
+ #EEFFGG#"
+)
+e
+w = ggplot(e)+
+  geom_text(aes(label = CatCharges, y = .07, x = 0, color = CatCharges), size = 5.5, hjust = 0.5, family ="title") +
+  geom_richtext(aes(label = glue("\\# Paid AI Tools: {n} <br> Price Range: {min} - {max}$"
+                                 ), y = 0.35, x = 0, color = CatCharges),
+                fill = NA, label.color = NA,
+                 size = 2.7, hjust = 0.5, family ="title") +
+  ggh4x::facet_manual(~Category, design = design) +
+  scale_y_continuous(limits = c(0, 0.5)) +
+  scale_color_manual(
+    values = c(
+      "$" = "#61bc84",
+      "$$" = "#ffffa1",
+      "$$$" = '#FF7F50'
+    )
+  ) +
+  labs(
+    title = title_text,
+    subtitle = subtitle_text,
+    caption = caption_text,
+    x = "",
+    y = ""
+  ) +
+  theme_minimal(base_size = 12) +
+  theme(
+    axis.text = element_blank(),
+    panel.grid = element_blank(),
+    legend.position = "none",
+    plot.background = element_rect(fill = bg_gradient),
+    plot.title = element_markdown(color = "white", family = "jost", hjust = 0.5,
+                                  margin = margin(t = 5, b = 10), size = 13),
+    plot.subtitle = element_markdown(size = 10, color = "white", 
+                                     lineheight = 1.2, family = "serif",
+                                     margin = margin(b = 10)),
+    plot.caption = element_markdown(color = "white", hjust = 0.5,
+                                    lineheight = 1.3, size = 8,
+                                    margin = margin(t = -5, b = 5)),
+    panel.spacing.y = unit(1.5, "lines"),
+    strip.text = element_markdown(color = "white", face = "bold", size = 12, family = "title"))
+
+ggsave(
+  "2024/day26/day26-cc.png", w, width =6, height = 4
+)
+
---
+++ 2024/day28/day28.R
@@ -0,0 +1,115 @@
+library(readr)
+library(dplyr)
+
+library(lubridate)
+library(ggplot2)
+library(glue)
+library(ggtext)
+library(showtext)
+library(sysfonts)
+library(ggimage)
+library(geomtextpath)
+
+
+sysfonts::font_add_google("Outfit", "title")
+sysfonts::font_add_google("Ubuntu Condensed", "uc")
+sysfonts::font_add_google("Jost", "jost")
+
+sysfonts::font_add('fb', '/home/stelios/Downloads/fontawesome-free-6.4.0-desktop/otfs/Font Awesome 6 Brands-Regular-400.otf')
+sysfonts::font_add('fs', '/home/stelios/Downloads/fontawesome-free-6.4.0-desktop/otfs/Font Awesome 6 Free-Solid-900.otf')
+
+
+showtext_auto()
+showtext::showtext_opts(dpi = 300)
+
+here::here("2024/day28")
+
+
+trends <- read_csv("2024/day28/AnalyticsTrends.csv", 
+                            skip = 1)
+
+d = trends |>
+  setNames(c("Month", "R", "SPSS", "MATLAB")) |>
+  mutate(
+    Month = lubridate::ym(Month),
+    Year = year(Month)
+  ) |>
+  dplyr::relocate(Year, .before = "Month") |>
+  select(-Month) |>
+  tidyr::pivot_longer(
+    cols = !c("Year")
+  ) |>
+  group_by(Year, name) |>
+  summarise(
+    mean = mean(value)
+  ) |>
+  mutate(
+    image = case_when(
+      name == "R" ~ "2024/day28/www/Rlang.png",
+      name == "SPSS" ~ "2024/day28/www/spss.png",
+      TRUE ~ "2024/day28/www/matlab.png"
+    )
+  )
+
+## Viz texts
+
+title_text = glue("Search Trends in Analytics Tools")
+subtitle_text = glue("Investigating people's interest in specific analytics software or programming language,
+                     I concluded that <br><b><span style='color:#019b98;'>R</span></b> (also known as <b><span style='color:#019b98;'>Rstats</span></b>) has finally prevailed in analytics field the last decade 
+                     over  <b><span style='color:#F18F01;'>MATLAB</span></b> &  <b><span style='color:#dd0025;'>SPSS</span></b><br>
+                     Of course we should take into consideration that Google's data are referring to search interest.
+                     Other <br> Statistical Software (EViews, JASP, jamovi etc.) have not getting much searches, so
+  data are not available.")
+caption_text = "30 Day Chart Challenge, Day 28 (2024)<br><b>Data:</b> Google Trends<br><span style='font-family:fb;'  >&#xf09b;</span> <b>stesiam</b>, 2024"
+
+
+bg_gradient <- grid::linearGradient(colours = rev(MetBrewer::met.brewer("Pillement")[5:6]))
+
+
+
+te = ggplot(d ,aes(x = Year, y  = mean, group = name, color = name)) +
+  geom_point(
+    data = d %>% filter(Year == 2024), size = 10, aes(x = Year, y = mean, group = name,
+                                                      color = name)) +
+  geom_line(lwd = 3) +
+  geom_textline(aes(x = Year, y = round(mean), 
+                    color = name, label = name, group = name), size = 5, 
+                fontface = 1, hjust = 0.21, vjust = -0.3, family = "title") +
+  geom_image( data = d %>% filter(Year == 2024), aes(x=Year,y=mean,image=image, color = NULL), asp=2.2)+
+  labs(
+    title = title_text,
+    subtitle = subtitle_text,
+    caption = caption_text,
+    y = "Search Interest"
+    ) +
+  scale_x_continuous(breaks = c(2005, 2010, 2015, 2020, 2024)) +
+  scale_y_continuous(n.breaks = 5,
+                     limits = c(0, 100)) +
+  scale_color_manual(
+    values = c(
+      "R" = "#019b98",
+      "MATLAB" = "#F18F01",
+      "SPSS" = "#dd0025"
+  )) +
+  theme_minimal(base_size = 12) +
+  theme(
+    text = element_text(color = "white"),
+    plot.title = element_markdown(color = "white", family = "jost", face = "bold",
+                                  hjust = 0.5),
+    plot.subtitle = element_markdown(family = "jost", size = 9.3, lineheight = 1.1),
+    plot.caption = element_markdown(lineheight = 1.2, size = 7),
+    plot.background = element_rect(fill = bg_gradient),
+    panel.background = element_rect(fill = "transparent", color = "transparent"),
+    legend.position = "none",
+    axis.text = element_text(color = "white", size = 10),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.grid = element_blank(),
+    axis.text.y = element_blank(),
+    axis.line.x = element_line(linewidth = 0.5)
+  )
+
+ggsave(
+  here::here("2024/day28/day28-cc.png"), te, width =6, height = 4
+)
+
